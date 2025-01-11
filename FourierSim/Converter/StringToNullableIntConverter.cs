9be11ad496f17114599d2ab96184d522c98a0821
Kai-Uwe Knappe <kaiuweknappe@gmail.com>
2025-01-11 21:28:03 +0100

    adding to last commit

--- FourierSim/Converter/StringToNullableIntConverter.cs
+++ FourierSim/Converter/StringToNullableIntConverter.cs
@@ -1,6 +1,25 @@
+using System.Globalization;
+using Avalonia.Data.Converters;
+
 namespace FourierSim.Converter;
 
-public class StringToNullableIntConverter
+/// <summary>
+/// used for validation between a Textbox and an int? Property 
+/// </summary>
+public class StringToNullableIntConverter : IValueConverter
 {
-    
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value is int intValue ? intValue.ToString() : string.Empty;   
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is string stringValue && int.TryParse(stringValue, out var result))
+        {
+            return result;
+        }
+
+        return null;
+    }
 }
